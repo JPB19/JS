6324ba0aca694d5714bc9ec70d01eaf929962b55
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2019-07-22 21:58:58 -0300

    subo clase 1 terminada

--- clase 1/js/app.js
+++ clase 1/js/app.js
@@ -1,56 +1,256 @@
-var string = "hola";
-var number = 25;
-var boolean = true;
+window.onload = function() {
 
-console.log(string);
-console.log(number);
-console.log(boolean);
+  var string = "hola";
+  var number = 25;
+  var boolean = true;
 
-var ironMan = {
+  console.log(string);
+  console.log(number);
+  console.log(boolean);
 
-  nombre: "IronMan",
-  equipo: "avengers",
-  poderes:["volar", "disparar misiles" , "laser"],
-  energia: 100,
-  getPoder : function(number){
-    return ironMan.poderes[number];
+  var ironMan = {
+
+    nombre: "IronMan",
+    equipo: "avengers",
+    poderes:["volar", "disparar misiles" , "laser"],
+    energia: 100,
+    getPoder : function(number){
+      return ironMan.poderes[number];
+    }
+  }
+
+  var hulk = {
+
+    nombre: "Hulk",
+    equipo: "avengers",
+    poderes:["aplastar", "gritar" , "golpear"],
+    energia: 100,
+    getPoder : function(number){
+      return hulk.poderes[number];
+    }
+  }
+
+
+  //console.log(ironMan.getPoder(0));
+  console.log(hulk);
+  // console.log(hulk.getPoder(numeropoder));
+
+  do {
+    var numeropoder = Math.floor(Math.random() * 3);
+    console.log("salió el número:", numeropoder);
+
+    if (numeropoder == 0) {
+      console.log("Poder:", ironMan.getPoder(0));
+      console.log("Descuenta:", 10);
+      console.log("Energia actual:", ironMan.energia);
+
+      ironMan.energia = ironMan.energia - 10;
+      console.log("Energia final:", ironMan.energia);
+    }
+    if (numeropoder == 1){
+      console.log("Poder:", ironMan.getPoder(1));
+      console.log("Descuenta:", 15);
+      console.log("Energia actual:", ironMan.energia);
+
+      ironMan.energia = ironMan.energia - 15;
+      console.log("Energia final:", ironMan.energia);
+    }
+
+    if (numeropoder == 2){
+      console.log("Poder:", ironMan.getPoder(0));
+      console.log("Descuenta:", 25);
+      console.log("Energia actual:", ironMan.energia);
+
+      ironMan.energia = ironMan.energia - 25;
+      console.log("Energia final:", ironMan.energia);
+    }
+
+  } while (ironMan.energia > 0);
+
+
+
+  do {
+    var numeropoder = Math.floor(Math.random() * 3);
+    console.log("salió el número:", numeropoder);
+
+    if (numeropoder == 0) {
+      console.log("Poder:", hulk.getPoder(0));
+      console.log("Descuenta:", 5);
+      console.log("Energia actual:", hulk.energia);
+
+      hulk.energia = hulk.energia - 5;
+      console.log("Energia final:", hulk.energia);
+    }
+    if (numeropoder == 1){
+      console.log("Poder:", hulk.getPoder(1));
+      console.log("Descuenta:", 25);
+      console.log("Energia actual:", hulk.energia);
+
+      hulk.energia = hulk.energia - 25;
+      console.log("Energia final:", hulk.energia);
+    }
+
+    if (numeropoder == 2){
+      console.log("Poder:", hulk.getPoder(0));
+      console.log("Descuenta:", 10);
+      console.log("Energia actual:", hulk.energia);
+
+      hulk.energia = hulk.energia - 10;
+      console.log("Energia final:", hulk.energia);
+    }
+
+  } while (hulk.energia > 0);
+
+
+  var misDatos= {
+    nombre: "Julian",
+    apellido: "Boschi",
+    dni: 34983375,
+    comidafavorita: "asado",
+    edad: 29,
+    saludar: function(){
+      console.log("Hola mi nombre es " + this.nombre + " " + this.apellido + "  y tengo " +this.edad + " años.");
+    }
+  }
+
+
+
+
+  for (var datos in misDatos) {
+      console.log(datos + ":" + misDatos[datos]);var string = "hola";
+  var number = 25;
+  var boolean = true;
+
+  console.log(string);
+  console.log(number);
+  console.log(boolean);
+
+  var ironMan = {
+
+    nombre: "IronMan",
+    equipo: "avengers",
+    poderes:["volar", "disparar misiles" , "laser"],
+    energia: 100,
+    getPoder : function(number){
+      return ironMan.poderes[number];
+    }
+  }
+
+  var hulk = {
+
+    nombre: "Hulk",
+    equipo: "avengers",
+    poderes:["aplastar", "gritar" , "golpear"],
+    energia: 100,
+    getPoder : function(number){
+      return hulk.poderes[number];
+    }
+  }
+
+
+  //console.log(ironMan.getPoder(0));
+  console.log(hulk);
+  // console.log(hulk.getPoder(numeropoder));
+
+  do {
+    var numeropoder = Math.floor(Math.random() * 3);
+    console.log("salió el número:", numeropoder);
+
+    if (numeropoder == 0) {
+      console.log("Poder:", ironMan.getPoder(0));
+      console.log("Descuenta:", 10);
+      console.log("Energia actual:", ironMan.energia);
+
+      ironMan.energia = ironMan.energia - 10;
+      console.log("Energia final:", ironMan.energia);
+    }
+    if (numeropoder == 1){
+      console.log("Poder:", ironMan.getPoder(1));
+      console.log("Descuenta:", 15);
+      console.log("Energia actual:", ironMan.energia);
+
+      ironMan.energia = ironMan.energia - 15;
+      console.log("Energia final:", ironMan.energia);
+    }
+
+    if (numeropoder == 2){
+      console.log("Poder:", ironMan.getPoder(0));
+      console.log("Descuenta:", 25);
+      console.log("Energia actual:", ironMan.energia);
+
+      ironMan.energia = ironMan.energia - 25;
+      console.log("Energia final:", ironMan.energia);
+    }
+
+  } while (ironMan.energia > 0);
+
+
+
+  do {
+    var numeropoder = Math.floor(Math.random() * 3);
+    console.log("salió el número:", numeropoder);
+
+    if (numeropoder == 0) {
+      console.log("Poder:", hulk.getPoder(0));
+      console.log("Descuenta:", 5);
+      console.log("Energia actual:", hulk.energia);
+
+      hulk.energia = hulk.energia - 5;
+      console.log("Energia final:", hulk.energia);
+    }
+    if (numeropoder == 1){
+      console.log("Poder:", hulk.getPoder(1));
+      console.log("Descuenta:", 25);
+      console.log("Energia actual:", hulk.energia);
+
+      hulk.energia = hulk.energia - 25;
+      console.log("Energia final:", hulk.energia);
+    }
+
+    if (numeropoder == 2){
+      console.log("Poder:", hulk.getPoder(0));
+      console.log("Descuenta:", 10);
+      console.log("Energia actual:", hulk.energia);
+
+      hulk.energia = hulk.energia - 10;
+      console.log("Energia final:", hulk.energia);
+    }
+
+  } while (hulk.energia > 0);
+
+
+  var misDatos= {
+    nombre: "Julian",
+    apellido: "Boschi",
+    dni: 34983375,
+    comidafavorita: "asado",
+    edad: 29,
+    saludar: function(){
+      console.log("Hola mi nombre es " + this.nombre + " " + this.apellido + "  y tengo " +this.edad + " años.");
+    }
+  }
+
+
+
+
+  for (var datos in misDatos) {
+      console.log(datos + ":" + misDatos[datos]);
+
+
+  }
+
+
+  console.log(misDatos.saludar());
+
+
+
+
+
+
+  var botonPrueba = document.getElementById('miBoton');
+
+  botonPrueba.onclick = function(){alert("Testeando el click")};
+
+
   }
 }
-
-var hulk = {
-
-  nombre: "Hulk",
-  equipo: "avengers",
-  poderes:["aplastar", "gritar" , "golpear"],
-  energia: 100,
-  getPoder : function(number){
-    return hulk.poderes[number];
-  }
-}
-
-
-//console.log(ironMan.getPoder(0));
-console.log(hulk);
-
-var numeropoder = Math.random(0 , 2);
-console.log(numeropoder);
-
-var numeropoder2= Math.floor(0 ,2);
-
-if (ironMan.getPoder(0)) {
-  ironMan.energia -10;
-}
-if (ironMan.getPoder(1)){
-  ironMan.energia - 15;
-}
-
-if (ironMan.getPoder(2)){
-  ironMan.energia - 25;
-}
-
-console.log(ironMan.getPoder(numeropoder));
-console.log(ironMan.getPoder(numeropoder2));
-console.log(hulk.getPoder(numeropoder));
-console.log(hulk.getPoder(numeropoder2));
-
-console.log(ironMan.energia);
